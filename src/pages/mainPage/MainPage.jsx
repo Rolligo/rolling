890f8ebcc167569e8adca4177b1821213641bbf2
K-Y-Hoo <bookopen1@gmail.com>
2023-11-04 18:01:09 +0900

    feat: 메인 페이지 초기 설정 및 라우트 설정

---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainPageDiv from "./components/MainPageDiv";
+
+const MainPage = () => {
+  return (
+    <MainPageDiv>
+      {/* 네비게이션 바 태그 여기에 위치해주세요! */}
+      hello world!
+    </MainPageDiv>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/mainPage/components/MainPageDiv.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const MainPageDiv = styled.main`
+  padding: 0 2.4rem;
+`;
+
+export default MainPageDiv;
