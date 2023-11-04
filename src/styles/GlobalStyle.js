fd5fd92dab9e9d287f3bdf6d115b9ac65733d69c
Geon <geonulim@gmail.com>
2023-11-04 16:32:05 +0900

    chore: 초기 설정 마무리

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "./reset";
+import { pretendard } from "assets/fonts/Pretendard";
+import { notoSans } from "assets/fonts/Noto-Sans";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  ${pretendard}
+  ${notoSans}
 `;
 
 export default GlobalStyle;
