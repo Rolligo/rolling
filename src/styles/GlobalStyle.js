2a50a7582381c5cb86b35bda4c3443aa29b80cee
Geon <geonulim@gmail.com>
2023-11-04 17:33:32 +0900

    fix: 폰트 경로 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "./reset";
-import { pretendard } from "assets/fonts/Pretendard";
-import { notoSans } from "assets/fonts/Noto-Sans";
+import { pretendard } from "assets/fonts/pretendard";
+import { notoSans } from "assets/fonts/noto-sans";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
