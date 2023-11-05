16dbdac8c2c0f61b3f6263465e7fa8aae8412a9e
asusia <asusia@naver.com>
2023-11-05 18:01:31 +0900

    feat: 드롭다운 컴포넌트 구현

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,63 @@
+import * as S from "./Dropdown.style";
+import arrowDown from "assets/icons/arrow_down.svg";
+import arrowUp from "assets/icons/arrow_top.svg";
+import { useEffect, useRef, useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(options[0]);
+  const ref = useRef(null);
+
+  const handleClickButton = (e) => {
+    e.stopPropagation();
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleClickListItem = (e) => {
+    setSelectedValue(e.target.innerText);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <S.DropdownContainer>
+      <S.ButtonContainer
+        active={isOpen}
+        error={isError}
+        onClick={handleClickButton}
+      >
+        <p>{selectedValue}</p>
+        {isOpen ? (
+          <img src={arrowUp} alt="위 화살표" />
+        ) : (
+          <img src={arrowDown} alt="아래 화살표" />
+        )}
+      </S.ButtonContainer>
+      {isError && <S.ErrorMessage>Error Message</S.ErrorMessage>}
+      {isOpen && (
+        <S.ListContainer ref={ref}>
+          {options.map((value, index) => (
+            <S.ListItem key={index} onClick={handleClickListItem}>
+              {value}
+            </S.ListItem>
+          ))}
+        </S.ListContainer>
+      )}
+    </S.DropdownContainer>
+  );
+}
+
+export default Dropdown;
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const RELATIONSHIP = ["친구", "지인", "동료", "관계"];
+
+export const FONT_SELECT = [
+  "Noto Sans",
+  "Pretendard",
+  "나눔명조",
+  "나눔손글씨 손편지체",
+];
