c164aec3b625fb00b88b34efd968bccbd87e7ae6
Geon <geonulim@gmail.com>
2023-11-05 22:14:17 +0900

    feat: 관계 배지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import GlobalStyle from "styles/GlobalStyle";
 
 function App() {
+  const style = {
+    display: "flex",
+    gap: "1rem",
+    padding: "5rem",
+  };
   return (
     <>
       <GlobalStyle />
