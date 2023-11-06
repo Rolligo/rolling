f42a51ad3e5d8aedbee9df1ee42d052d88b12090
arky02 <kyean07@naver.com>
2023-11-06 16:17:27 +0900

    fix: App에 쓸때없는 코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import ArrowButton from "components/StyledButtons/ArrowButton";
 import GlobalStyle from "styles/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <ArrowButton right />
-      </div>
+      <div></div>
     </>
   );
 }
