b61c9d3069963a17731c034d896f5a3f2b1f47e0
K-Y-Hoo <bookopen1@gmail.com>
2023-11-06 18:12:18 +0900

    feat: dangerouslySetInnerHTML을 이용한 입력값 내보내기

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import * as S from "./TextEditor.style";
+
 const TextEditor = () => {
+  const [text, setText] = useState("");
+  function onChangeContents(contents) {
+    setText(contents);
+  }
+
   return (
     <>
       <S.TextEditorContainer>
-        <ReactQuill />
+        <ReactQuill onChange={onChangeContents} />
+      </S.TextEditorContainer>
+      <S.TextEditorContainer>
+        <div dangerouslySetInnerHTML={{ __html: text }} />
       </S.TextEditorContainer>
     </>
   );
