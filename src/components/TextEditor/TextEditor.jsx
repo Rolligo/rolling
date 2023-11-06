610d9a6c5af7d30fcd521ccc3b12774e0109d738
K-Y-Hoo <bookopen1@gmail.com>
2023-11-06 20:59:31 +0900

    feat: 폰트 선택값을 prop로 설정, useRef로 폰트 적용

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -1,11 +1,12 @@
-import React, { useMemo, useState } from "react";
-import ReactQuill, { Quill } from "react-quill";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import * as S from "./TextEditor.style";
 import DOMPurify from "dompurify";
 import "./Editor.css";
-const TextEditor = () => {
+const TextEditor = ({ fontFamily = "Pretendard" }) => {
   const [text, setText] = useState("");
+  const quillRef = useRef(null);
 
   const formats = [
     "font",
@@ -49,6 +50,15 @@ const TextEditor = () => {
     setText(contents);
   }
 
+  //페이지 컴포넌트에서 폰트 선택 state가 변경될 때 handleFontChange함수 호출
+  function handleFontChange(ref) {
+    ref.current.style.fontFamily = fontFamily;
+  }
+
+  useEffect(() => {
+    handleFontChange(quillRef);
+  }, [fontFamily]);
+
   return (
     <>
       <S.TextEditorContainer>
@@ -57,15 +67,17 @@ const TextEditor = () => {
           modules={modules}
           formats={formats}
           placeholder="I am your reach text editor."
+          ref={quillRef}
         />
       </S.TextEditorContainer>
       <S.TextEditorContainer>
         <div
+          ref={quillRef}
           dangerouslySetInnerHTML={{
             __html: DOMPurify.sanitize(text),
           }}
           // 윗줄의 div 태그는 기능 동작을 위한 예시입니다.
-          // 입력된 값을 사용할 컴포넌트에서 text 변수를 prop으로 받고, 위의 div 태그의 내용처럼 사용하시면 됩니다.
+          // 입력된 값을 사용할 컴포넌트에서 text, quillRef 변수를 prop으로 받고, 위의 div 태그의 내용처럼 사용하시면 됩니다.
         />
       </S.TextEditorContainer>
     </>
