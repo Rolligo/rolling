8da1364220df5612a9f85fe65ee5f71ba6bebaf8
K-Y-Hoo <bookopen1@gmail.com>
2023-11-06 18:05:57 +0900

    feat: React-quill 라이브러리 초기 설정

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -1,80 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import * as S from "./TextEditor.style";
-import Bold from "assets/images/Bold.svg";
-import Italic from "assets/images/Italic.svg";
-import UnderLine from "assets/images/UnderLine.svg";
 const TextEditor = () => {
-  const [text, setText] = useState("");
-  const [isBoldClicked, setIsBoldClicked] = useState(false);
-  const [isItalicClicked, setIsItalicClicked] = useState(false);
-  const [isUnderLineClicked, setIsUnderLineClicked] = useState(false);
-  const textRef = useRef(null);
-  console.log("Rendering");
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
-  function handleBoldClick(e) {
-    e.preventDefault();
-    setIsBoldClicked(!isBoldClicked);
-  }
-
-  function handleItalicClick(e) {
-    e.preventDefault();
-    setIsItalicClicked(!isItalicClicked);
-  }
-
-  function handleUnderLineClick(e) {
-    e.preventDefault();
-    setIsUnderLineClicked(!isUnderLineClicked);
-  }
-
-  useEffect(() => {
-    if (isBoldClicked) {
-      textRef.current.style.fontWeight = "bold";
-    } else {
-      textRef.current.style.fontWeight = "400";
-    }
-  }, [isBoldClicked]);
-
-  useEffect(() => {
-    if (isItalicClicked) {
-      textRef.current.style.fontStyle = "italic";
-    } else {
-      textRef.current.style.fontStyle = "normal";
-    }
-  }, [isItalicClicked]);
-
-  useEffect(() => {
-    if (isUnderLineClicked) {
-      textRef.current.style.textDecoration = "underline";
-    } else {
-      textRef.current.style.textDecoration = "none";
-    }
-  }, [isUnderLineClicked]);
-
   return (
-    <S.TextEditorContainer>
-      <S.EditContainer>
-        <S.Button onClick={(e) => handleBoldClick(e)}>
-          <img src={Bold} />
-        </S.Button>
-        <S.Button onClick={(e) => handleItalicClick(e)}>
-          <img src={Italic} />
-        </S.Button>
-        <S.Button onClick={(e) => handleUnderLineClick(e)}>
-          <img src={UnderLine} />
-        </S.Button>
-      </S.EditContainer>
-      <S.TextContainer
-        ref={textRef}
-        placeholder="I am your reach text editor."
-        type="text-area"
-        value={text}
-        onChange={handleChange}
-      />
-    </S.TextEditorContainer>
+    <>
+      <S.TextEditorContainer>
+        <ReactQuill />
+      </S.TextEditorContainer>
+    </>
   );
 };
 
--- src/components/TextEditor/TextEditor.style.js
+++ src/components/TextEditor/TextEditor.style.js
@@ -7,41 +7,10 @@ export const TextEditorContainer = styled.div`
   flex-direction: column;
   width: 100%;
   padding: 0rem 0rem 1.6rem 0.1rem;
-  border-radius: 0.8rem;
-  border: 0.1rem solid ${COLORS.GRAY_300};
-  max-width: 71rem;
+  max-width: 70.5rem;
   ${onTablet} {
   }
 
   ${onPc} {
   }
-`;
-
-export const EditContainer = styled.div`
-  background: ${COLORS.GRAY_200};
-  width: 100%;
-  height: 5rem;
-  display: flex;
-  align-items: center;
-  padding: 0 1.5rem;
-  gap: 0.5rem;
-`;
-
-export const TextContainer = styled.textarea`
-  border: none;
-  resize: none;
-  min-height: 20rem;
-  padding: 1.5rem 1.5rem;
-  color: ${COLORS.GRAY_900};
-  font-family: Pretendard;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 26px; /* 162.5% */
-  letter-spacing: -0.16px;
-`;
-
-export const Button = styled.button`
-  width: 2.4rem;
-  height: 2.4rem;
 `;
