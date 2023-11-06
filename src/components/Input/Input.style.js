8e211324fd70867688cbdce2edc7745cbfaefd4d
summerkimm <summerkim0825@gmail.com>
2023-11-06 15:54:45 +0900

    Refactor: 폰트 사이즈 수정

--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -2,27 +2,27 @@ import styled, { css } from "styled-components";
 import { COLORS } from "styles/palette";
 
 export const InputContainer = styled.input`
-  width: 20rem;
-  padding: 0.75rem 1rem;
-  border-radius: 0.5rem;
-  border-width: 0.06rem;
+  width: 32rem;
+  padding: 1.2rem 1.6rem;
+  border-radius: 0.8rem;
+  border-width: 0.1rem;
   border-style: solid;
   border-color: ${({ error }) => error ? COLORS.ERROR : COLORS.GRAY_300};
   background-color: ${COLORS.WHITE};
   color: ${({ error }) => error ? COLORS.GRAY_900 : COLORS.GRAY_500};
-  font-size: 1rem;
+  font-size: 1.6rem;
   font-style: normal;
   font-weight: 400;
   line-height: 162.5%;
-  letter-spacing: -0.01rem;
+  letter-spacing: -0.016rem;
 
   &:focus {
-    border-width: 0.13rem;
+    border-width: 0.2rem;
     border-color: ${COLORS.GRAY_500};
     color: ${COLORS.GRAY_900};
   }
   &:active {
-    border-width: 0.13rem;
+    border-width: 0.2rem;
     border-color: ${COLORS.GRAY_700};
     color: ${COLORS.GRAY_900};
   }
@@ -38,10 +38,10 @@ export const InputContainer = styled.input`
 `;
 export const InputError = styled.p`
   color: ${COLORS.ERROR};
-  font-size: 0.75rem;
+  font-size: 1.2rem;
   font-style: normal;
   font-weight: 400;
   line-height: 150%;
-  letter-spacing: -0.00375rem;
-  margin-top: 0.25rem;
+  letter-spacing: -0.006rem;
+  margin-top: 0.4rem;
 `;  
