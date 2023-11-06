884edf2b0809fb955fc8a1eb058c11d249069303
summerkimm <summerkim0825@gmail.com>
2023-11-06 15:27:24 +0900

    Refactor: placeholder를  prop으로  추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import GlobalStyle from "styles/GlobalStyle";
-import Input from "components/Input";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <Input />
-      </div>
+      <div></div>
     </>
   );
 }
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,12 @@
 import * as S from "./Input.style";
 import { useState } from "react";
 
-function Input() {
+function Input({ placeholder }) {
   const [isError, setIsError] = useState(false);
 
   return(
     <>
-      <S.InputContainer type="text" error={isError} placeholder="placeholder" />
+      <S.InputContainer type="text" error={isError} placeholder={placeholder} />
       {isError && <S.InputError>Error message</S.InputError>}
     </>
   );
