7e05316c42173af99505d77ae8ebb64d433a3d43
K-Y-Hoo <bookopen1@gmail.com>
2023-11-07 14:19:19 +0900

    feat: PostMessagePage 라우터 및 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "styles/GlobalStyle";
 import MainPage from "pages/MainPage";
+import PostMessagePage from "pages/PostMessagePage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/post/1/message" element={<PostMessagePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PostMessagePage/PostMessagePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./PostMessagePage.style";
+
+const PostMessagePage = () => {
+  return <div>PostMessagePage</div>;
+};
+
+export default PostMessagePage;
