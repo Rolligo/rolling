bcfbc5250801c0d57355974c82c243efbe4f662b
arky02 <kyean07@naver.com>
2023-11-08 12:14:04 +0900

    feat: onClick prop으로 받도록 기능 추가

--- src/components/StyledButtons/RoundAddButton/RoundAddButton.jsx
+++ src/components/StyledButtons/RoundAddButton/RoundAddButton.jsx
@@ -1,9 +1,9 @@
 import * as S from "./RoundAddButton.style";
 import addIcon from "assets/images/icons/add-icon.svg";
 
-function RoundAddButton({ width = "5.6rem", height = "5.6rem" }) {
+function RoundAddButton({ width = "5.6rem", height = "5.6rem", onClick }) {
   return (
-    <S.RoundBtnContainer width={width} height={height}>
+    <S.RoundBtnContainer width={width} height={height} onClick={onClick}>
       <img src={addIcon}></img>
     </S.RoundBtnContainer>
   );
