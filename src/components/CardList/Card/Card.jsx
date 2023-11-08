804e6048e8df1eeb27c30b43e404d6611ea12383
asusia <asusia1111@gmail.com>
2023-11-09 06:23:39 +0900

    feat: 카드 확대(모달) 기능 구현

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -3,36 +3,54 @@ import * as S from "./Card.style";
 import deleteIcon from "assets/images/icons/delete-icon.svg";
 import { RELATIONSHIP } from "utils/constants";
 import formatDate from "utils/formatDate";
-import { useEffect } from "react";
+import { useState } from "react";
+import Modal from "components/Modal";
 
 function Card({ item, isEditMode }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const relationship = RELATIONSHIP[item.relationship];
   const date = formatDate(item.createdAt);
 
-  useEffect(() => {
-    console.log(item);
-  });
+  const handleClickCard = () => {
+    if (window.innerWidth > 768) {
+      setModalIsOpen(true);
+    }
+  };
+
+  const handleClickBackdropOrButton = () => {
+    setModalIsOpen(false);
+  };
 
   return (
     <>
-      <S.Profile>
-        <S.ProfileImage src={item.profileImageURL} alt="프로필 이미지" />
-        <S.ProfileText>
-          <S.Sender>
-            From.<span>{item.sender}</span>
-          </S.Sender>
-          <Relationship relationship={relationship} />
-        </S.ProfileText>
-      </S.Profile>
-      {isEditMode && (
-        <S.IconContainer>
-          <img src={deleteIcon} alt="삭제 버튼" />
-        </S.IconContainer>
+      <div onClick={handleClickCard}>
+        <S.Profile>
+          <S.ProfileImage src={item.profileImageURL} alt="프로필 이미지" />
+          <S.ProfileText>
+            <S.Sender>
+              From.<span>{item.sender}</span>
+            </S.Sender>
+            <Relationship relationship={relationship} />
+          </S.ProfileText>
+        </S.Profile>
+        {isEditMode && (
+          <S.IconContainer>
+            <img src={deleteIcon} alt="삭제 버튼" />
+          </S.IconContainer>
+        )}
+        <S.Content>
+          <S.Message font={item.font}>{item.content}</S.Message>
+          <S.Date>{date}</S.Date>
+        </S.Content>
+      </div>
+      {modalIsOpen && (
+        <Modal
+          close={handleClickBackdropOrButton}
+          item={item}
+          relationship={relationship}
+          date={date}
+        />
       )}
-      <S.Content>
-        <S.Message font={item.font}>{item.content}</S.Message>
-        <S.Date>{date}</S.Date>
-      </S.Content>
     </>
   );
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,38 @@
+import Relationship from "components/Badges/Relationship";
 import * as S from "./Modal.style";
 import PrimaryButton from "components/StyledButtons/PrimaryButton";
 
-function Modal({ setOpenModal, name, badge, date }) {
-
-  const handleModalClose = () => {
-    setOpenModal(false);
-  }
-
+function Modal({ close, item, relationship, date }) {
   return (
     <>
-      <S.ModalBackdrop onClick={handleModalClose}/>
+      <S.ModalBackdrop onClick={close} />
       <S.ModalContainer>
         <S.ModalHeader>
           <S.ProfileWrapper>
-            <S.ProfileImg></S.ProfileImg>
+            <S.ProfileImg src={item.profileImageURL}></S.ProfileImg>
             <S.ProfileInfo>
-              <div>From. <S.Name>{name}</S.Name></div>
-              <div>{badge}</div>
+              <div>
+                From. <S.Name>{item.sender}</S.Name>
+              </div>
+              <Relationship relationship={relationship} />
             </S.ProfileInfo>
           </S.ProfileWrapper>
           <S.ProfileDate>{date}</S.ProfileDate>
         </S.ModalHeader>
         <S.ModalContent>
-          <S.ModalTextField>
-            코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요! 코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!
-            코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요! 코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!
-          </S.ModalTextField>
-          <PrimaryButton onClick={handleModalClose} width="12rem" margin="4rem auto 1rem" smallSize >확인</PrimaryButton>
+          <S.ModalTextField font={item.font}>{item.content}</S.ModalTextField>
+          <PrimaryButton
+            onClick={close}
+            width="12rem"
+            margin="4rem auto 1rem"
+            smallSize
+          >
+            확인
+          </PrimaryButton>
         </S.ModalContent>
       </S.ModalContainer>
     </>
   );
-};
+}
 
 export default Modal;
