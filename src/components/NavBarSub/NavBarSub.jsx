6e377745670487617ceb528cd4408e519727c3d7
summerkimm <summerkim0825@gmail.com>
2023-11-08 23:52:42 +0900

    Feat: 모바일 사이즈 NavBarSub 구현

--- src/components/NavBarSub/NavBarSub.jsx
+++ src/components/NavBarSub/NavBarSub.jsx
@@ -1,8 +1,37 @@
 import * as S from "./NavBarSub.style";
+import Emoji from "components/Badges/Emoji";
+// import OutlinedButton from "components/StyledButtons/OutlinedButton";
+// import addIcon from "assets/images/icons/add-icon.svg";
+import addEmoticon from "assets/images/icons/add-emoticon.svg";
+import ArrowIcon from "assets/images/icons/arrow_down.svg";
+import OutlinedButton from "components/StyledButtons/OutlinedButton";
 
 function NavBarSub() {
   return (
-    <></>
+    <S.Wrapper>
+      <S.Header>
+        <S.ToContainer>To. Ashley Kim</S.ToContainer>
+      </S.Header>
+      <S.HeaderService>
+        <S.EmojiContainer>
+          <S.EmojiWrapper>
+            <Emoji />
+            <Emoji />
+            <Emoji />
+          </S.EmojiWrapper>
+          <S.Button><img src={ArrowIcon} /></S.Button>
+        </S.EmojiContainer>
+        <S.ButtonContainer>
+          <OutlinedButton width="3.6rem" height="3.6rem"  smallSize>
+            <img src={addEmoticon} />
+          </OutlinedButton>
+          <S.Border />
+          <OutlinedButton width="3.6rem" height="3.6rem"  smallSize>
+            <img src={addEmoticon} />
+          </OutlinedButton>
+        </S.ButtonContainer>
+      </S.HeaderService>
+    </S.Wrapper>
   );
 }
 export default NavBarSub;
--- src/components/NavBarSub/NavBarSub.style.js
+++ src/components/NavBarSub/NavBarSub.style.js
@@ -1,4 +1,61 @@
 import styled from "styled-components";
 import { COLORS } from "styles/palette";
-import { onPc, onTablet } from "styles/mediaQuery";
+import { FONT_STYLE } from "styles/fontStyle";
 
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  padding: 1.2rem 2rem;
+  border-bottom: 0.1rem solid ${COLORS.GRAY_200};
+`;
+
+export const ToContainer = styled.div`
+  width: 100%;
+  color: ${COLORS.GRAY_800};
+  ${FONT_STYLE.BOLD_18};
+`;
+
+export const HeaderService = styled.div`
+  width: 100%;
+  padding: 0.8rem 2rem;
+  display: flex;
+  gap: 0.2rem;
+  border-bottom: 0.1rem solid ${COLORS.GRAY_200};
+
+`;
+
+export const EmojiWrapper = styled.div`
+  display: flex;
+  gap: 0.8rem;
+`;
+
+export const Button = styled.button`
+  width: 3.6rem;
+  height: 3.6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EmojiContainer = styled.div`
+  display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  justify-content: center;
+  align-item: center;
+`;
+
+export const Border = styled.div`
+  background-color: ${COLORS.GRAY_200};
+  width: 1px;
+  height: 2.8rem;
+  margin: auto 0;
+`;
