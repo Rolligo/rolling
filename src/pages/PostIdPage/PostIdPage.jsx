cd59f7d1d3a550f9729c9a86597bfe791755fd36
asusia <asusia1111@gmail.com>
2023-11-09 16:06:30 +0900

    fix: 카드 디자인 오류 수정

--- src/pages/PostIdPage/PostIdPage.jsx
+++ src/pages/PostIdPage/PostIdPage.jsx
@@ -2,7 +2,7 @@ import CardList from "components/CardList";
 import * as S from "./PostIdPage.style";
 import NavBar from "components/NavBar";
 import OutlinedButton from "components/StyledButtons/OutlinedButton";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import PrimaryButton from "components/StyledButtons/PrimaryButton";
 import useRequest from "hooks/useRequest";
 
@@ -10,13 +10,33 @@ function PostIdPage() {
   const { id } = useParams();
   const { data } = useRequest({
     deps: [id],
-    url: `https://rolling-api.vercel.app/1-4/recipients/${id}/`,
+    url: `recipients/${id}/`,
   });
 
   const location = useLocation();
   const currentPath = location.pathname;
   const editURL = `${currentPath}/edit`;
   const isEditMode = currentPath.endsWith("/edit");
+  const navigate = useNavigate();
+
+  const deletePaper = async () => {
+    try {
+      const response = await fetch({
+        url: `/recipients/${id}/`,
+        method: "DELETE",
+      });
+      if (response.status !== 204) {
+        throw new Error("롤링 페이퍼 삭제 실패");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteClick = () => {
+    deletePaper();
+    navigate("/list");
+  };
 
   return (
     <S.Background
@@ -31,10 +51,16 @@ function PostIdPage() {
         {isEditMode ? (
           <>
             <S.DeleteButton>
-              <PrimaryButton>삭제하기</PrimaryButton>
+              <PrimaryButton onClick={handleDeleteClick}>
+                삭제하기
+              </PrimaryButton>
             </S.DeleteButton>
             <S.DeleteButtonOnPc>
-              <PrimaryButton smallSize width="9.2rem">
+              <PrimaryButton
+                onClick={handleDeleteClick}
+                smallSize
+                width="9.2rem"
+              >
                 삭제하기
               </PrimaryButton>
             </S.DeleteButtonOnPc>
