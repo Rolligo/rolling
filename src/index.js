de13cc25d22b4f8bb1ff5795af9294fa5ac9cc40
Geon <geonulim@gmail.com>
2023-11-14 17:30:32 +0900

    fix: FOUT 문제 해결

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "assets/fonts/pretendard/font.css";
+import "assets/fonts/noto-sans/font.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
